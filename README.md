49473408493780e36de2159001916f3e4d4e5adf
JonLuca DeCaro <jdecaro@usc.edu>
2018-03-08 12:37:12 -0800

    2fa scripts

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ CLIENT_ID = "client_id"
 CLIENT_SECRET = "secret"
 USERNAME = "user"
 PASSWORD = "pass"
+USE_2FA = False
 ```
 
 Alternatively, you can edit these values within clear_unmod directly.
@@ -27,3 +28,13 @@ Alternatively, you can edit these values within clear_unmod directly.
 To run the script:
 
 `python clear_unmod.py`
+
+
+## Additional notes
+
+To get your client ID and secret, create [an app here](https://www.reddit.com/prefs/apps). Select Script under what type of app you'll be creating
+
+
+### 2FA
+
+If you are using 2 factor authentication, you must set USE_2FA to True, and input your one time passcode at runtime. 
--- clear_unmod.py
+++ clear_unmod.py
@@ -1,19 +1,22 @@
 import praw
 
 try:
-	#try to read from config.py
+	# try to read from config.py
 	from account import *
 except:
 	CLIENT_ID = "client_id"
 	CLIENT_SECRET = "secret"
 	USERNAME = "user"
 	PASSWORD = "pass"
+	USE_2FA = False
 
+if USE_2FA:
+	token = input("Input 2FA Token here: ")
+	PASSWORD = PASSWORD + ":" + token
 
 # Generate reddit instance from details in account
 r = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent="approve_unmod", username=USERNAME,
-	password=PASSWORD)
-
+                password=PASSWORD)
 
 # Iterate over all subreddits the user mods
 for sub in r.user.moderator_subreddits(limit=None):
