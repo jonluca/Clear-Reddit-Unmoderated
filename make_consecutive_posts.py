b123fbd02471cb4aa9028dea975fa676e749b191
JonLuca DeCaro <jdecaro@usc.edu>
2018-10-18 17:51:18 -0400

    added make consecutive, fixed 2fa

--- make_consecutive_posts.py
+++ make_consecutive_posts.py
@@ -13,26 +13,21 @@ except:
 
 if USE_2FA:
 	token = input("Input 2FA Token here: ")
-	PASSWORD = PASSWORD + ":" + token
+	PASSWORD = PASSWORD + ":" + str(token)
 
 # Generate reddit instance from details in account
 r = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent="approve_unmod", username=USERNAME,
                 password=PASSWORD)
+for i in range(1,14):
+	title = "Daredevil Discussion Thread - S03E%02d" % i
 
-def approve_post(post):
-	post.mod.approve()
-	print(post.permalink)
+	body = """This thread is for discussion of Daredevil S02E%02d."
 
-# Iterate over all subreddits the user mods
-	# Get all unmoderated posts
-# Approve them and print ids
-def clear():
-	for post in r.subreddit('damnthatsinteresting').mod.modqueue(limit=None):
-		Thread(target=approve_post,args=[post]).start()
-		time.sleep(0.25)
-	clear()
+**DO NOT** post spoilers in this thread for any subsequent episodes. Doing so will result in a ban.
 
-clear()
+[Episode %d Discussion]()""" % (i, i+1)
+
+	submission = r.subreddit('Defenders').submit(title = title, selftext=body, send_replies=False)
 
 # for item in reddit.subreddit('churning').mod.modqueue(limit=None):
 # 	Thread(target=approve_post,args=[post]).start()		
