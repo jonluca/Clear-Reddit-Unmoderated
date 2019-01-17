e38f9a72a00b7d7b450e92cb2b009655151f8d4c
JonLuca DeCaro <jdecaro@usc.edu>
2019-01-17 10:23:52 -0800

    Better thread creation script for MarvelStudios

--- make_consecutive_posts.py
+++ make_consecutive_posts.py
@@ -1,33 +1,45 @@
 import praw
-from threading import Thread
-import time
+
 try:
-	# try to read from config.py
-	from account import *
+    # try to read from config.py
+    from account import *
 except:
-	CLIENT_ID = "client_id"
-	CLIENT_SECRET = "secret"
-	USERNAME = "user"
-	PASSWORD = "pass"
-	USE_2FA = False
+    CLIENT_ID = "client_id"
+    CLIENT_SECRET = "secret"
+    USERNAME = "user"
+    PASSWORD = "pass"
+    USE_2FA = False
 
 if USE_2FA:
-	token = input("Input 2FA Token here: ")
-	PASSWORD = PASSWORD + ":" + str(token)
+    token = input("Input 2FA Token here: ")
+    PASSWORD = PASSWORD + ":" + str(token)
 
 # Generate reddit instance from details in account
 r = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent="approve_unmod", username=USERNAME,
                 password=PASSWORD)
-for i in range(1,14):
-	title = "Daredevil Discussion Thread - S03E%02d" % i
+prev_link = ""
+links = []
+for i in range(13, 0, -1):
+    title = "The Punisher Discussion Thread - S02E%02d" % i
 
-	body = """This thread is for discussion of Daredevil S02E%02d."
+    body = """This thread is for discussion of The Punisher S02E%02d.
 
 **DO NOT** post spoilers in this thread for any subsequent episodes. Doing so will result in a ban.
 
-[Episode %d Discussion]()""" % (i, i+1)
+[Episode %d Discussion](%s)""" % (i, i + 1, prev_link)
 
-	submission = r.subreddit('Defenders').submit(title = title, selftext=body, send_replies=False)
+    submission = r.subreddit('Defenders').submit(title=title, selftext=body, send_replies=False)
+    submission.mod.approve()
+    submission.disable_inbox_replies()
+    submission.mod.distinguish('yes')
+    submission.mod.lock()
+    prev_link = submission.shortlink
+    links.append('* [Episode %d Discussion](%s)' % (i, prev_link))
+
+links.reverse()
+for link in links:
+    print(link + '\n')
+
 
 # for item in reddit.subreddit('churning').mod.modqueue(limit=None):
-# 	Thread(target=approve_post,args=[post]).start()		
+# 	Thread(target=approve_post,args=[post]).start()
